8b05f72166c7d2d6bb028d16488fc62e924ef075
이정우 <ljw8124@gmail.com>
2022-11-07 13:52:31 +0900

    mocha beforeEach 적용/ 픽스처 수정하기

--- refactoring01/testSample_ex01.js
+++ refactoring01/testSample_ex01.js
@@ -1,4 +1,5 @@
 const assert = require('assert');
+const expect = require('chai').expect;
 
 class Province {
   constructor(doc) {
@@ -56,6 +57,10 @@ class Province {
     });
     return result;
   }
+  
+  get demandValue() {
+    return this.satisfiedDemand * this.price;
+  }
 }
 
 class Producer {
@@ -87,7 +92,7 @@ function sampleProvinceData() {
   return {
     name: 'Asia',
     producers: [
-      {name: 'Byzantium', cose: 10, production: 9},
+      {name: 'Byzantium', cost: 10, production: 9},
       {name: 'Attalia', cost: 12, production: 10},
       {name: 'Sinope', cost: 10, production: 6},
     ],
@@ -96,8 +101,9 @@ function sampleProvinceData() {
   };
 }
 
-//생산 부족분을 제대로 계산하는지 확인하는 테스트
-//모카프레임워크
+// 생산 부족분을 제대로 계산하는지 확인하는 테스트
+// 모카프레임워크
+// 모카 프레임워크는 어느 테스트가 실패했는지, 실패원인을 추론할 수 있는 단서를 제공한다.
 describe('province', function() {
   it('shortfall ', function() {
     const asia = new Province(sampleProvinceData());  //픽스처 설정
@@ -107,3 +113,61 @@ describe('province', function() {
 
 // ES6 를 사용하였다면 따로 실행필요
 // npm install @babel/register --save-dev
+
+// 실패해야 할 상황에서는 반드시 실패하게 만들자
+// 자주 테스트하라. 작성 중인 코드는 최소한 몇 분 간격으로 테스트하고, 적어도 하루에 한 번은 전체 테스트를 돌려보자.
+
+// 테스트는 위험 요인을 중심으로 만들어야 한다.
+// 와벽하게 만드느라 테스트를 수행하지 못하느니, 불완전한 테스트라도 작성해 실행하는 게 낫다.
+
+// expect 는 chai 에서 추가 필요
+/*
+describe('province2', function() {
+  // 이런식으로 공유 픽스처를 생성하는 것은 좋지 않다.
+  // const 는 상수라는 의미인데, 나중에 asia 를 수정할 필요가 생기면 오류가 발생하기 때문이다.
+  // const asia = new Province(sampleProvinceData());
+  it('shortfall', function() {
+    const asia = new Province(sampleProvinceData());
+    expect(asia.shortfall).equal(5);
+  });
+  it('profit', () => {
+    const asia = new Province(sampleProvinceData());
+    expect(asia.profit).equal(230);
+  })
+});
+ */
+
+// mocha 에서 describe
+// before() - 모든 테스트가 실행되지 너에 모든 테스트를 describe
+// after() - 한 번 실행한 후 모든 테스트를 describe
+// beforeEach() - 테스트가 실행되기 전에 각각 describe
+// afterEach() - 테스트가 실행 된 이후 각각 describe
+
+describe('province2', () => {
+  
+  let asia;
+  beforeEach(() => {
+    asia = new Province(sampleProvinceData());
+  });
+  it('shortfall', () => {
+    expect(asia.shortfall).equal(5);
+  });
+  it('profit', () => {
+    expect(asia.profit).equal(230);
+  });
+  
+  // setter 테스트
+  // beforeEach 블록에서 설정한 표준 픽스처를 취해서 테스트를 수행하고, 제대로 처리했는지 검증한다.
+  // '설정-실행-검증' 또는 '조건-발생-결과' 또는 '준비-수행-단언' 등으로 불림
+  it('change production', () => {
+    asia.producers[0].production = 20;
+    expect(asia.shortfall).equal(-6);
+    expect(asia.profit).equal(292);
+  });
+  // 위 코드에서는 it 에서 두가지 사항을 검증하고 있는데,
+  // 일반적으로는 it 하나당 하나의 사항을 검증하는 것이 좋음
+  // because, 앞 쪽 검증을 통과하지 않으면 나머지 검증은 실행되지도 않고 실패하기 때문이다.
+});
+
+
+
