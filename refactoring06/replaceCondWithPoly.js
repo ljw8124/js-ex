691da977a64aa72aea84cd2789a1978e1b22651b
Joungwoo Lee <ljw8124@gmail.com>
2022-11-21 20:52:47 +0900

    조건부 로직을 다형성으로 바꾸기

--- refactoring06/replaceCondWithPoly.js
+++ refactoring06/replaceCondWithPoly.js
@@ -224,15 +224,10 @@ class Rating {
   // 위 에서 복잡했던 중국 여행 기록을 따로 함수로 추출
   get voyageAndHistoryLengthFactor() {
     let result = 0;
-    if(this.voyage.zone === "중국" && this.hasChinaHistory)  {
-      result += 3;
-      if(history.length > 10) result += 1;
-      if(history.length > 12) result += 1;
-      if(history.length > 18) result += 1;
-    } else {
-      if(history.length > 8) result += 1;
-      if(voyage.length > 14) result -= 1;
-    }
+
+    if(history.length > 8) result += 1;
+    if(voyage.length > 14) result -= 1;
+
     return result;
   }
 
@@ -286,4 +281,13 @@ class ExperiencedChinaRating extends Rating {
     const result = super.captainHistoryRisk - 2;
     return Math.max(result, 0);
   }
+  get voyageAndHistoryLengthFactor() {
+    let result = 0;
+    result += 3;
+    if(this.history.length > 10) result += 1;
+    if(this.voyage.length > 12) result += 1;
+    if(this.voyage.length > 18) result -= 1;
+    return result;
+  }
+
 }
