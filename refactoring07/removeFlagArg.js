7efc89ca69f4bbc2fda8b08f693741ad4521c241
Joungwoo Lee <ljw8124@gmail.com>
2022-11-24 10:43:11 +0900

    플래그 인수 제거하기

--- refactoring07/removeFlagArg.js
+++ refactoring07/removeFlagArg.js
@@ -11,22 +11,22 @@ aShipment.deliveryDate = deliveryDate(anOrder, false);
 
 // 위와 같이 호출하고 있다면, 두번째 파라미터가 어떤 의미인지 모르게 된다.
 
-function deliveryDate(anOrder, isRush) {
-  if(isRush) {
-    let deliveryTime;
-    if(["MA", "CT"].includes(anOrder.deliveryState)) deliveryTime = 1;
-    else if(["NY", "NH"].includes(anOrder.deliveryState)) deliveryTime = 2;
-    else deliveryTime = 3;
-    return anOrder.placedOn.plusDays(1 + deliveryTime);
-  }
-  else {
-    let deliveryTime;
-    if(["MA", "CT"].includes(anOrder.deliveryState)) deliveryTime = 2;
-    else if(["NY", "NH"].includes(anOrder.deliveryState)) deliveryTime = 3;
-    else deliveryTime = 4;
-    return anOrder.placedOn.plusDays(2 + deliveryTime);
-  }
-}
+// function deliveryDate(anOrder, isRush) {
+//   if(isRush) {
+//     let deliveryTime;
+//     if(["MA", "CT"].includes(anOrder.deliveryState)) deliveryTime = 1;
+//     else if(["NY", "NH"].includes(anOrder.deliveryState)) deliveryTime = 2;
+//     else deliveryTime = 3;
+//     return anOrder.placedOn.plusDays(1 + deliveryTime);
+//   }
+//   else {
+//     let deliveryTime;
+//     if(["MA", "CT"].includes(anOrder.deliveryState)) deliveryTime = 2;
+//     else if(["NY", "NH"].includes(anOrder.deliveryState)) deliveryTime = 3;
+//     else deliveryTime = 4;
+//     return anOrder.placedOn.plusDays(2 + deliveryTime);
+//   }
+// }
 
 // 위 함수는 '조건문 분해하기'를 적용할 수 있다.
 function deliveryDate(anOrder, isRush) {
